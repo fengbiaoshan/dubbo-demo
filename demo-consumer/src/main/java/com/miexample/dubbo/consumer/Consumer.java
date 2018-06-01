de8ebbcd00b4bedae2b55cdea529ab7b3230c78f
reck <fengbiaoshan@163.com>
2018-06-01 15:08:48 +0800

    demo basically finished

---
+++ demo-consumer/src/main/java/com/miexample/dubbo/consumer/Consumer.java
@@ -0,0 +1,18 @@
+package com.miexample.dubbo.consumer;
+
+import com.miexample.dubbo.common.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
+        context.start();
+        // obtain proxy object for remote invocation
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        // execute remote invocation
+        String hello = demoService.sayHello("world");
+        // show the result
+        System.out.println(hello);
+    }
+}
